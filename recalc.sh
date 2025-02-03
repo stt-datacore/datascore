6ac2896605498b32beebe96cd971b88f08d31085
Nathaniel Moschkin <ironywrit@gmail.com>
2025-02-03 18:23:04 -0500

    bug fixes.

--- recalc.sh
+++ recalc.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 npm run shipcalc-fresh
-node build/scripts/scoring
+node build/datascore/scripts/scoring
 zip ./battle.zip ./battle_run_cache.json && mv ./battle.zip ../scripts/data
+cd ../scripts
+git commit . -m "battle run cache regeneration"
+git push
+cd ../datascore
