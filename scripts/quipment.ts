71617b1890e310d0627fa3ee9ddac2607941b5a1
Nathaniel Moschkin <ironywrit@gmail.com>
2025-04-10 19:58:35 -0400

    remove dependency on unreleased website version

--- scripts/quipment.ts
+++ scripts/quipment.ts
@@ -1,7 +1,7 @@
 import { CrewMember, QuipmentDetails } from '../../website/src/model/crew';
 import { BuffStatTable } from '../../website/src/utils/voyageutils';
 import { EquipmentItem } from '../../website/src/model/equipment';
-import { calcQLots, sortCrewByQuipment } from '../../website/src/utils/equipment';
+import { calcQLots } from '../../website/src/utils/equipment';
 import { getPossibleQuipment, ItemWithBonus } from '../../website/src/utils/itemutils';
 import CONFIG from '../../website/src/components/CONFIG';
 import { skillSum } from '../../website/src/utils/crewutils';
@@ -206,3 +206,95 @@ export function scoreQuipment(crew: CrewMember, quipment: ItemWithBonus[], buffs
 
     return { qpower, vpower, bpower, gpower, avg: 0, symbol: crew.symbol, qprice, vprice, bprice, gprice };
 }
+
+
+export function sortCrewByQuipment(roster: CrewMember[], pstMode: boolean | 2 | 3, index: number | string, reverse?: boolean, tiebreaker?: (a: CrewMember, b: CrewMember) => number) {
+	const mul = reverse ? -1 : 1;
+	if (pstMode === true && typeof index === 'number') {
+		roster.sort((a, b) => mul * skoComp(a, b, index) || (tiebreaker ? (mul * tiebreaker(a, b)) : 0));
+	}
+	else if (pstMode === 2 && typeof index === 'number') {
+		roster.sort((a, b) => mul * multiComp(a, b, index) || (tiebreaker ? (mul * tiebreaker(a, b)) : 0));
+	}
+	else if (typeof index === 'string') {
+		roster.sort((a, b) => mul * qpComp(a, b, index) || (tiebreaker ? (mul * tiebreaker(a, b)) : 0));
+	}
+}
+
+export function qpComp(a: CrewMember, b: CrewMember, skill: string) {
+    if (!a.best_quipment!.aggregate_by_skill[skill]) return -1;
+    else if (!b.best_quipment!.aggregate_by_skill[skill]) return 1;
+    else return a.best_quipment!.aggregate_by_skill[skill] - b.best_quipment!.aggregate_by_skill[skill];
+};
+
+export function skoComp(a: CrewMember, b: CrewMember, skill_idx: number) {
+    if (skill_idx >= a.skill_order.length) {
+        return -1;
+    }
+    else if (skill_idx >= b.skill_order.length) {
+        return 1;
+    }
+    else {
+        return a.best_quipment!.aggregate_by_skill[a.skill_order[skill_idx]] - b.best_quipment!.aggregate_by_skill[b.skill_order[skill_idx]];
+    }
+};
+
+export function multiComp(a: CrewMember, b: CrewMember, combo_id: number) {
+    if (combo_id === 0) {
+        if (a.best_quipment_1_2 && b.best_quipment_1_2) {
+            return a.best_quipment_1_2.aggregate_power - b.best_quipment_1_2.aggregate_power;
+        }
+        else if (a.best_quipment_1_2) {
+            return 1;
+        }
+        else if (b.best_quipment_1_2) {
+            return -1;
+        }
+    }
+    else if (combo_id === 1) {
+        if (a.best_quipment_1_3 && b.best_quipment_1_3) {
+            return a.best_quipment_1_3.aggregate_power - b.best_quipment_1_3.aggregate_power;
+        }
+        else if (a.best_quipment_1_3) {
+            return 1;
+        }
+        else if (b.best_quipment_1_3) {
+            return -1;
+        }
+    }
+    else if (combo_id === 2) {
+        if (a.best_quipment_2_3 && b.best_quipment_2_3) {
+            return a.best_quipment_2_3.aggregate_power - b.best_quipment_2_3.aggregate_power;
+        }
+        else if (a.best_quipment_2_3) {
+            return 1;
+        }
+        else if (b.best_quipment_2_3) {
+            return -1;
+        }
+    }
+    else if (combo_id === 3) {
+        if (a.best_quipment_3 && b.best_quipment_3) {
+            return a.best_quipment_3.aggregate_power - b.best_quipment_3.aggregate_power;
+        }
+        else if (a.best_quipment_3) {
+            return 1;
+        }
+        else if (b.best_quipment_3) {
+            return -1;
+        }
+    }
+    else if (combo_id === 4) {
+        if (a.best_quipment_top && b.best_quipment_top) {
+            return a.best_quipment_top.aggregate_power - b.best_quipment_top.aggregate_power;
+        }
+        else if (a.best_quipment_top) {
+            return 1;
+        }
+        else if (b.best_quipment_top) {
+            return -1;
+        }
+    }
+
+    return 0;
+};
