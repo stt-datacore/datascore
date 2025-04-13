98a4fc278da38aeb8cc7da05f9f9ab7648ba05b9
Nathaniel Moschkin <ironywrit@gmail.com>
2025-04-12 23:54:58 -0400

    non-deterministic setting.

--- calc.sh
+++ calc.sh
@@ -4,4 +4,4 @@ unzip ../scripts/data/battle.zip
 npm run precalculate
 npm run shipcalc
 node build/datascore/scripts/scoring
-zip ./battle.zip ./battle_run_cache.json && mv ./battle.zip ../scripts/data
+zip -X ./battle.zip ./battle_run_cache.json && mv ./battle.zip ../scripts/data
