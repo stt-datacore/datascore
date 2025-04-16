7ecc15535d884e71a70ee7cdf76421eadc3f398f
Nathaniel Moschkin <ironywrit@gmail.com>
2025-04-15 22:03:20 -0400

    better trait matching for events

--- scripts/eventscoring.ts
+++ scripts/eventscoring.ts
@@ -4,6 +4,7 @@ import { GameEvent } from '../../website/src/model/player';
 import { CrewMember } from '../../website/src/model/crew';
 import { getVariantTraits } from '../../website/src/utils/crewutils';
 import { getEventData } from '../../website/src/utils/events';
+import { TraitNames, TranslationSet } from '../../website/src/model/traits';
 
 const DEBUG = process.argv.includes("--debug");
 
@@ -15,9 +16,69 @@ interface EventScoring {
     score: number;
 }
 
-export function eventScoring() {
 
+const series = ['discovery', 'voyager', 'enterprise', 'stt originals', 'deep space 9', 'deep space nine', 'next generation', 'lower decks', 'strange new worlds'];
+const short = ['dsc', 'voy', 'ent', 'original', 'ds9', 'ds9', 'tng', 'low', 'snw'];
+
+export function getEventBonusTrait(event: GameEvent, crew: CrewMember[], trait_names: TraitNames) {
+    let keys = Object.keys(trait_names);
+    let values = Object.values(trait_names);
+    let traits = {} as {[key: string]: number};
+    const words = event.bonus_text.replace('Crew Bonus: ', '').replace('Bonus: ', '').replace(' crew', '').replace('(Ship/Crew)', '').replace('(Ship)', '').replace('(Crew)', '').replace(/\sor\s/, ',').split(',').filter(word => word !== '').map(s => s.trim().replace(" Crew", ""));
+    words.forEach((w, idx) => {
+        if (idx === words.length - 1) {
+            let fc = crew.find(ff => ff.short_name === w);
+            if (fc) return;
+        }
+        if (w === 'Xenoanthropologists') {
+            traits['xenoanthropology'] ??= 0;
+            traits['xenoanthropology']++;
+            //etraits.push('xenoanthropology');
+            return;
+        }
+        else if (w === 'Thieves') {
+            traits['thief'] ??= 0;
+            traits['thief']++;
+            //etraits.push('thief');
+            return;
+        }
+        else if (w.includes('Tribble')) {
+            traits['tribbled'] ??= 0;
+            traits['tribbled']++;
+            //etraits.push('tribbled');
+            return;
+        }
+
+        let f = values.findIndex(trait => trait === w || trait === w.slice(0, w.length - 1) || w.includes(trait));
+        if (f === -1) {
+            f = keys.findIndex(trait => trait === w || trait === w.slice(0, w.length - 1) || w.includes(trait));
+        }
+        if (f !== -1 || ['animated'].includes(w.toLowerCase())) {
+            traits[keys[f]] ??= 0;
+            traits[keys[f]]++;
+            //etraits.push(keys[f]);
+        }
+        else {
+            f = series.findIndex(seri => w.toLowerCase().includes(seri));
+            if (f !== -1) {
+                traits[short[f]] ??= 0;
+                traits[short[f]]++;
+                //etraits.push(short[f]);
+            }
+            f = short.findIndex(seri => w.toLowerCase().includes(seri));
+            if (f !== -1) {
+                traits[short[f]] ??= 0;
+                traits[short[f]]++;
+                //etraits.push(short[f]);
+            }
+        }
+    });
+    return traits;
+}
+
+export function eventScoring() {
     const inputCrew = JSON.parse(fs.readFileSync(`${STATIC_PATH}crew.json`, 'utf-8')) as CrewMember[];
+    const translations = JSON.parse(fs.readFileSync(`${STATIC_PATH}translation_en.json`, 'utf-8')) as TranslationSet;
 
     const publicTraits = [...new Set(inputCrew.map(c => c.traits).flat())];
 
@@ -61,6 +122,13 @@ export function eventScoring() {
                         bonus_traits[bc] += 1;
                     }
                 }
+            }
+            else {
+                let traits = getEventBonusTrait(event, inputCrew, translations.trait_names);
+                Object.entries(traits).forEach(([trait, count]) => {
+                    bonus_traits[trait] ??= 0;
+                    bonus_traits[trait] += count;
+                });
             }
         }
     }
