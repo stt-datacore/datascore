1a0deea8de611f3007a6c6943aae20e1d4b7edd0
Nathaniel Moschkin <ironywrit@gmail.com>
2025-04-27 22:51:14 -0400

    add "---nocommit" option to recalc

--- recalc.sh
+++ recalc.sh
@@ -8,6 +8,9 @@ npm run shipcalc-fresh
 node build/datascore/scripts/scoring
 npm run eventstats
 zip -X ./battle.zip ./battle_run_cache.json && mv ./battle.zip ../scripts/data
+if [ "$1" == "--nocommit" ]; then
+    exit 0;
+fi
 cd ../scripts
 git commit . -m "battle run cache regeneration"
 git push
