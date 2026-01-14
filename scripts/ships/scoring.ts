20e5d3e0aadf2dee5bc45626a3d250781ab9ebb4
Nathaniel Moschkin <ironywrit@gmail.com>
2026-01-14 01:01:57 -0500

    avoid trigger avoidance altogether

--- scripts/ships/scoring.ts
+++ scripts/ships/scoring.ts
@@ -522,7 +522,7 @@ export const getStaffedShip = (ships: Ship[], crew: CrewMember[], ship: string |
             if (bs.crew) continue;
 
             let d1 = filtered.find(f => {
-                if (f.action.ability?.condition && !pass && !evasion_needed) return false;
+                //if (f.action.ability?.condition && !pass && !evasion_needed) return false;
                 if (((!ignore_skill && !f.skill_order.some(s => dataskills.includes(s))) || used.includes(f.symbol))) return false;
                 if (c && c.symbol === f.symbol) return true;
                 if (c && pass === 0) {
