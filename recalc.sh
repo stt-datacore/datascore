80d36cfb18f1f7e09c909c1007a2ad33492d5f9d
Nathaniel Moschkin <ironywrit@gmail.com>
2026-02-14 11:08:32 -0500

    recalc script

--- recalc.sh
+++ recalc.sh
@@ -4,9 +4,9 @@ git checkout master
 git pull --no-rebase
 cd ../datascore
 npm run precalculate
-if [ "$1" == "--only-meta" || "$2" == "--only-meta" ]; then
+if [[ "$1" == "--only-meta" || "$2" == "--only-meta" ]]; then	
     npm run shipcalc-fresh-meta
-elif [ "$1" == "--only-cache" || "$2" == "--only-cache" ]; then
+elif [[ "$1" == "--only-cache" || "$2" == "--only-cache" ]]; then
     npm run shipcalc-fresh-cache
 else
     npm run shipcalc-fresh
@@ -15,7 +15,7 @@ npm run calc
 npm run eventstats
 zip -X ./battle.zip ./battle_run_cache.json && mv ./battle.zip ../scripts/data
 zip -X ./meta.zip ./battle_meta_cache.json && mv ./meta.zip ../scripts/data
-if [ "$1" == "--nocommit" || "$2" == "--nocommit" ]; then
+if [[ "$1" == "--nocommit" || "$2" == "--nocommit" ]]; then
     exit 0;
 fi
 cd ../scripts
